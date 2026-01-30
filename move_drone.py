5d8df7e9980ed6a50a8e92919b19e237217dcd23
PetarKrtalic <petar.krtalic@fer.unizg.hr>
2026-01-30 20:34:10 +0100

    Fix trajectory issues

--- move_drone.py
+++ move_drone.py
@@ -28,6 +28,26 @@ def move_drones():
 
     #cf.takeoff(1.0, 2.0)
     timeHelper.sleep(2.5)
+    cf1.goTo(
+    [cf1.position[0],
+    cf1.position[1],
+    cf1.position[2]],
+    0.0, 0.1
+)
+    cf2.goTo(
+    [cf2.position[0],
+    cf2.position[1],
+    cf2.position[2]],
+    0.0, 0.1
+)
+    cf3.goTo(
+    [cf3.position[0],
+    cf3.position[1],
+    cf3.position[2]],
+    0.0, 0.1
+    )
+
+    timeHelper.sleep(0.2)
 
     cf1.uploadTrajectory(0, 0, traj1)
     cf2.uploadTrajectory(0, 0, traj2)
